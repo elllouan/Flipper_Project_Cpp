c93b606393df015099292558f926937c1854dcf1
elllouan <524115@student.fontys.nl>
2023-07-30 22:27:57 +0200

    Tried to implement own lookAt matrix (not working)

--- src/camera.cpp
+++ src/camera.cpp
@@ -2,6 +2,48 @@
 
 #include <utility>
 
+// @brief Implements my own lookAt matrix to view
+// @note All args are not required to be positive values, they can be negative
+// @param n near plan coordinate
+// @param f far plan distance
+// @param r right screen coordinate
+// @param l left screen coordinate
+// @param t top screen coordinate
+// @param b bottom screen coordinate
+static glm::mat4
+Clip(float n, float f, float r, float l, float t, float b)
+{
+    float xe = -2*n/(r-l);
+    float ye = -2*n/(t-b);
+    float ze1 = (r+l)/(r-l);
+    float ze2 = (t+b)/(t-b);
+    float A = -(f+n)/(f-n);
+    float B = -2*n*f/(f-n);
+    glm::mat4 clip = glm::mat4(xe,  0,  ze1,    0,  // 1st column
+                                0, ye,  ze2,    0,  // 2nd column
+                                0,  0,    A,    B,  // 3rd column
+                                0,  0,-1.0f,    0); // 4th column
+    return glm::transpose(clip);
+}
+
+static glm::mat4
+MyLookAt(const glm::vec3 &target, const glm::vec3 &y, const glm::vec3 &position)
+{
+    glm::vec3 direction = glm::normalize(position-target);
+    glm::vec3 right = glm::normalize(glm::cross(y,direction));
+    glm::vec3 up = glm::normalize(glm::cross(direction, right));
+    glm::mat4 base = glm::mat4(right.x, up.x, direction.x, 0,
+                               right.y, up.y, direction.y, 0,
+                               right.z, up.z, direction.z, 0,
+                                     0,    0,           0, 1);
+    glm::mat4 translate = glm::mat4(          1,           0,           0, 0,
+                                              0,           1,           0, 0,
+                                              0,           0,           1, 0,
+                                    -position.x, -position.y, -position.z, 1);
+    glm::mat4 view = glm::transpose(base)*glm::transpose(translate);
+    return view;
+}
+
 void
 Camera::__SetBase()
 {
@@ -30,6 +72,7 @@ Camera::Camera(glm::vec3 &initialPosition,
 glm::mat4 Camera::CreateView()
 {
     m_view = glm::lookAt(m_position, m_target, y);
+    // m_view = MyLookAt(m_target, y, m_position);
     return m_view;
 }
 
@@ -68,6 +111,7 @@ glm::mat4 Camera::ChangeView(const glm::vec3 &newPosition, const glm::vec3 &newT
 glm::mat4 Camera::UpdateView()
 {
     m_view = glm::lookAt(m_position, m_target, y);
+    // m_view = MyLookAt(m_target, y, m_position);
     __SetBase();
     return m_view;
 }
@@ -91,12 +135,14 @@ void Camera::MoveForward(float timeFrame)
     // m_direction is by my definition m_position - m_target (<0)
     glm::vec3 delta = m_speed*timeFrame*glm::normalize(m_direction);
     m_position -= delta;
+    m_target -= delta;
 }
 
 void Camera::MoveBackwards(float timeFrame)
 {
     glm::vec3 delta = m_speed*timeFrame*glm::normalize(m_direction);
     m_position += delta;
+    m_target += delta;
 }
 
 
@@ -146,5 +192,4 @@ void Camera::NodView(float frameTime, float limitAngle, bool right)
                             projXZ*sin(-pitch)*cos(yawZ));
     
     m_target = m_position-m_direction;
-    // m_view = glm::lookAt(m_position, m_target, y);
 }
